cd0a9f8923a4d2d41d024523d7f5d6b448261828
Gondi László <laszlo.zsolt.gondi@gmail.com>
2022-10-11 15:49:59 +0200

    Add test in main.java

--- src/hu/petrik/autok/Main.java
+++ src/hu/petrik/autok/Main.java
@@ -1,7 +1,22 @@
 package hu.petrik.autok;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Main {
+    static List<Auto> autoList = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Porsche p1 = new Porsche(60);
+        Tesla t1 = new Tesla();
+        Tesla t2 = new Tesla();
+        Porsche p2 = new Porsche(120);
+        Porsche p3 = new Porsche(200);
+
+        autoList.add(p1);
+        autoList.add(t1);
+        autoList.add(t2);
+        autoList.add(p2);
+        autoList.add(p3);
     }
 }
